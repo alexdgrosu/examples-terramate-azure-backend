6ae37e785b483dc27814141ace45449717240b47
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2024-03-07 19:48:02 +0100

    feat: Add sample workloads stacks

---
+++ stacks/workloads/dev/_generated_terraform_backend.tf
@@ -0,0 +1,12 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+terraform {
+  required_version = "~> 1.7"
+  backend "azurerm" {
+    container_name       = "tfstate"
+    resource_group_name  = "rg-tmaz-tf-backend"
+    storage_account_name = "sttmaztfbackend001"
+    key                  = "tmaz/stacks/workloads/dev/terraform.tfstate"
+    snapshot             = true
+  }
+}
---
+++ stacks/workloads/prod/stack.tm.hcl
@@ -0,0 +1,5 @@
+stack {
+  name        = "prod"
+  description = "prod"
+  id          = "0ac87b4c-784a-4c23-8acb-ae1e938c0df7"
+}
